0c5dd864bdada4e5ce0cda2dc993f3776e652b67
Marcus Silveira <marcusvinicius.767.mv@gmaiil.com>
2023-02-01 09:33:49 -0300

    resumo dos valores booleanos

---
+++ Fundamentos/booleanos.js
@@ -0,0 +1,16 @@
+// Verificar o boolean
+
+let = isAtivo = true
+
+console.log(!isAtivo) // ! nega a variável
+
+console.log(!!isAtivo) // !! retorna o valor booleano dela
+
+console.log(!!0) // !! retorna o valor booleano dela
+
+console.log(!!'') // !! str vazia retorna false
+
+// || opereador OU
+
+const nome = '' //false
+console.log(nome || 'Desconhecido')
